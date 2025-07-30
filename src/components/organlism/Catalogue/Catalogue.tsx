33bc5d8d22ac1ecf3af78aca93f8f6422620697c
GiselaMoreno <gmoreno@onrentx.com>
2025-07-30 12:37:35 -0600

    Delete src/components/organlism/Catalogue

--- src/components/organlism/Catalogue/Catalogue.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Catalogue({slug}: {slug?: string}) {
-    return(
-        <div className="catalog-page">
-            <h1>Catálogo</h1>
-            <p>Slug: {slug}</p>
-        </div>
-    )
-}
