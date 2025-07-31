c913dbc83ef4545c3a97e63102579987b5a9355d
GiselaMoreno <gmoreno@onrentx.com>
2025-07-31 14:28:58 -0600

    Delete src/components/organlism/CatalogueContainer.tsx

--- src/components/organlism/CatalogueContainer.tsx
+++
@@ -1,8 +0,0 @@
-// components/organism/Catalogue/CatalogueContainer.tsx
-import { sampleData } from "./sampleCatalogueData"; // Simulación temporal
-import CatalogueList from "./CatalogueList";
-
-export default function CatalogueContainer() {
-  const filteredData = sampleData; // Lógica real se agregará luego
-  return <CatalogueList items={filteredData} />;
-}
