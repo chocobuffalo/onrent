3dbb6de4b47fcb990838accddc5cee69f386cc1a
Juan Carlos Avila <juanvs23@gmail.com>
2025-08-19 16:44:21 -0400

    actualizando desde el repo

--- src/app/(frontend)/(buyprocess)/nuevo-proyecto/page.tsx
+++ src/app/(frontend)/(buyprocess)/nuevo-proyecto/page.tsx
@@ -1,6 +1,10 @@
 import ReserveForm from "@/components/organism/buyprocess/reserveform/reserveForm";
 import NewProjectForm from "@/components/organism/NewProjectForm/NewProjectForm";
 
+export const metadata = {
+    title: "Nuevo Proyecto",
+    description: "Crea un nuevo proyecto y reserva tu espacio.",
+}
 
 export default function NuevoProyecto() {
     
@@ -8,7 +12,7 @@ export default function NuevoProyecto() {
         <section className="py-[70px] px-4">
             <div className="mx-auto container">
             <h1 className="text-2xl font-bold  mb-6">Nuevo Proyecto</h1>
-                <ReserveForm />
+                <NewProjectForm />
             </div>
        </section>        
     );
--- src/hooks/frontend/buyProcess/useNewProjectForm.tsx
+++ src/hooks/frontend/buyProcess/useNewProjectForm.tsx
@@ -1,20 +1,57 @@
 "use client";
 import { useUIAppSelector } from "@/libs/redux/hooks";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { use, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+
+
+const Schema = Yup.object({
+    name: Yup.string().required("Nombre es requerido"),
+  location: Yup.string().required("Ubicación es requerida"),
+  estimated_duration: Yup.string().required("Duración estimada es requerida"),
+  start_date: Yup.string().required("Fecha de inicio es requerida"),
+  end_date: Yup.string().required("Fecha de finalización es requerida"),
+  responsible_name: Yup.string().required("Nombre del responsable es requerido"),
+  work_schedule: Yup.string().required("Horario de trabajo es requerido"),
+  site_manager: Yup.string().required("Nombre del encargado de obra es requerido"),
+  manager_phone: Yup.string().required("Teléfono del encargado de obra es requerido"),
+  work_type: Yup.string().required("Tipo de trabajo es requerido"),
+  terrain_type: Yup.string().required("Tipo de terreno es requerido"),
+  access_terrain_condition: Yup.string().required("Condición de acceso al terreno es requerida"),
+  access_notes: Yup.string(),
+  has_reserve_space: Yup.boolean().required(),
+  extra_requirements: Yup.string(),
+  observations: Yup.string(),
+  state: Yup.string().default("planning"),
+  resguardo_files: Yup.array().of(Yup.string())
+})
 
 export default function useNewProjectForm() {
-    const {userID} = useUIAppSelector(state => state.filters);
+const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    resolver: yupResolver(Schema),
+    mode: "onChange",
+  });
     const session = useSession();
     console.log(session);
        const router = useRouter();
        useEffect(() => {
             if( typeof window !== "undefined"){
-                if (userID === null  && session.status !== "authenticated") {
+                if (session.status !== "authenticated") {
                     router.push("/iniciar-session");
                 }
             }
-       }, [userID, session.status]);
-       return {userID};
+       }, [session.status]);
+       return {
+           register,
+           handleSubmit,
+           errors,
+           isValid
+       };
 }
